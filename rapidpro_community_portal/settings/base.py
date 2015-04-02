9509574c6eac378e66d2fb6122ef7187eba0f7a3
Erin Mullaney <emullaney@caktusgroup.com>
2015-04-02 13:58:40 -0400

    adding demo site temporarily

--- rapidpro_community_portal/settings/base.py
+++ rapidpro_community_portal/settings/base.py
@@ -140,6 +140,7 @@ INSTALLED_APPS = (
     'wagtail.wagtailredirects',
     'wagtail.wagtailforms',
     'wagtail.wagtailsites',
+    'demo',
 )
 
 # A sample logging configuration. The only tangible logging
