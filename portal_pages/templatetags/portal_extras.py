967b9d0a63ad8c8857b3bd86e3a8c559131a6ed2
Erin Mullaney <emullaney@caktusgroup.com>
2015-05-12 14:59:24 -0400

    refs #13 adjust comment

--- portal_pages/templatetags/portal_extras.py
+++ portal_pages/templatetags/portal_extras.py
@@ -4,5 +4,5 @@ register = template.Library()
 
 @register.filter
 def make_unique(value, unique_var): # Only one argument.
-    """Converts a string into all lowercase"""
+    """Make the queryset unique by unique_var, sort by unique_var"""
     return value.distinct(unique_var).order_by(unique_var)
