e9a1603389b6b16235b8edb08fd7d5404b090a33
Erin Mullaney <emullaney@caktusgroup.com>
2015-05-13 14:06:02 -0400

    wip: test for #13

--- portal_pages/tests.py
+++ portal_pages/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.utils import timezone
+
+from accounts.models import RapidProUser
+from portal_pages.templatetags.portal_extras import make_unique
 
 # Create your tests here.
+
+class TagTests(TestCase):
+    # test direct primary key on an object
+    # try to use models like django groups
+
+    user1 = RapidProUser.objects.create(
+        username="jane@villa.com",
+        first_name="Jane",
+        last_name="Villanueva",
+        is_staff=False,
+        is_active=True
+        )
+    user2 = RapidProUser.objects.create(
+        username="xo@villa.com",
+        first_name="Xo",
+        last_name="Villanueva",
+        is_staff=False,
+        is_active=True
+        )
+
+    print(user1.username)
