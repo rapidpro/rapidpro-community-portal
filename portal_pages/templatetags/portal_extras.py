d7c4dd08f36e80f599103dcd87f0621e143e86c0
Erin Mullaney <emullaney@caktusgroup.com>
2015-05-13 16:45:25 -0400

    refs #13 added test for template tag make_unique, make_unique getattr modification

--- portal_pages/templatetags/portal_extras.py
+++ portal_pages/templatetags/portal_extras.py
@@ -5,7 +5,7 @@ register = template.Library()
 @register.filter
 def make_unique(value, unique_var): # Only one argument.
     """Make the queryset unique by unique_var, sort by unique_var"""
-    if 'distinct' in dir(value): # Need to check so that this does not break in Preview mode in Wagtail
+    if hasattr(value, "distinct"): # Need to check so that this does not break in Preview mode in Wagtail
         return value.distinct(unique_var).order_by(unique_var)
     else:
         return value
--- portal_pages/tests.py
+++ portal_pages/tests.py
@@ -7,22 +7,23 @@ from portal_pages.templatetags.portal_extras import make_unique
 # Create your tests here.
 
 class TagTests(TestCase):
-    # test direct primary key on an object
-    # try to use models like django groups
 
-    user1 = RapidProUser.objects.create(
-        username="jane@villa.com",
-        first_name="Jane",
-        last_name="Villanueva",
-        is_staff=False,
-        is_active=True
-        )
-    user2 = RapidProUser.objects.create(
-        username="xo@villa.com",
-        first_name="Xo",
-        last_name="Villanueva",
-        is_staff=False,
-        is_active=True
-        )
+    def test_make_unique_filter(self):
+        user1 = RapidProUser.objects.create(
+            username="jane@villa.com",
+            first_name="Jane",
+            last_name="Villanueva",
+            is_staff=False,
+            is_active=True
+            )
+        user2 = RapidProUser.objects.create(
+            username="xo@villa.com",
+            first_name="Xo",
+            last_name="Villanueva",
+            is_staff=False,
+            is_active=True
+            )
 
-    print(user1.username)
+        users_distinct_last = make_unique(RapidProUser.objects.all(), "last_name")
+
+        self.assertEqual(users_distinct_last.count(), 1)
