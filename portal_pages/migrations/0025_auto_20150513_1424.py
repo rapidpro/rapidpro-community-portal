30f7f08027df884e6691add24c16bc9653685307
Erin Mullaney <emullaney@caktusgroup.com>
2015-05-13 14:25:53 -0400

    adding m2m fields to snippets

---
+++ portal_pages/migrations/0025_auto_20150513_1424.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('portal_pages', '0024_auto_20150512_1426'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='expertise',
+            options={'ordering': ('name',), 'verbose_name_plural': 'expertise'},
+        ),
+    ]
