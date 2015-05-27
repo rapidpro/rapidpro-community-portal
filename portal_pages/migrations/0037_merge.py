fdacc447d33bb89e0616580ddd6af80fd78336c3
Erin Mullaney <emullaney@caktusgroup.com>
2015-05-27 16:26:28 -0400

    adding merge migration file

---
+++ portal_pages/migrations/0037_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('portal_pages', '0032_auto_20150527_1610'),
+        ('portal_pages', '0036_cmspage_top_image'),
+    ]
+
+    operations = [
+    ]
