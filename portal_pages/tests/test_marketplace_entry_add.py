ee44020a26fa0146f68935e1f82a5f7ff081c885
Erin Mullaney <emullaney@caktusgroup.com>
2015-06-19 12:16:25 -0400

    refss #45 minor test fix

--- portal_pages/tests/test_marketplace_entry_add.py
+++ portal_pages/tests/test_marketplace_entry_add.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from wagtail.wagtailcore.models import Page, Site
 
 from portal_pages.models import MarketplaceIndexPage, MarketplaceEntryPage
-from portal_pages.views import get_marketplace_entry
+from portal_pages.views import submit_marketplace_entry
 
 # Create your tests here.
 # Basing off of tests found here
