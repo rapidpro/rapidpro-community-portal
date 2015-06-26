0988249502b4dea4f9e4ebc8d59aa8d705374925
Erin Mullaney <emullaney@caktusgroup.com>
2015-06-26 09:54:08 -0400

    merge migration for latest updates

---
+++ portal_pages/migrations/0045_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('portal_pages', '0043_auto_20150625_0926'),
+        ('portal_pages', '0044_auto_20150625_0856'),
+    ]
+
+    operations = [
+    ]
