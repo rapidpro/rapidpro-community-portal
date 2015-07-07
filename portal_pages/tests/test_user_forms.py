09566e95b136c704a634e36206ffa50637d0ee1e
Erin Mullaney <emullaney@caktusgroup.com>
2015-07-07 14:46:22 -0400

    refs #185 url variable adjustment, test class rename

--- portal_pages/tests/test_user_forms.py
+++ portal_pages/tests/test_user_forms.py
@@ -11,7 +11,7 @@ from portal_pages.models import (
     )
 
 
-class MarketplaceEntryPageAddTests(TestCase):
+class UserFormTests(TestCase):
 
     def setUp(self):
         # Wagtail's default site comes with a Root (1) and Home Page (2)
@@ -26,7 +26,7 @@ class MarketplaceEntryPageAddTests(TestCase):
                 slug="marketplace",
                 live=True,
             ))
-        self.marketplace_submission_slug = '/%s/submit-marketplace-entry/' % self.marketplace_index_page.slug
+        self.marketplace_submission_url = self.marketplace_index_page.url + self.marketplace_index_page.reverse_subpage('submit')
 
         self.marketplace_entry_form_data = { 
             'date_start': '2015-01-01',
@@ -44,7 +44,7 @@ class MarketplaceEntryPageAddTests(TestCase):
                 slug="stories",
                 live=True,
             ))
-        self.case_study_submission_slug = '/%s/submit-case-study/' % self.case_study_index_page.slug
+        self.case_study_submission_url = self.case_study_index_page.url + self.case_study_index_page.reverse_subpage('submit')
 
         self.case_study_form_data = { 
             'date': '2015-01-01',
@@ -64,7 +64,7 @@ class MarketplaceEntryPageAddTests(TestCase):
                 slug="blog",
                 live=True,
             ))
-        self.blog_submission_slug = '/%s/submit-blog/' % self.blog_index_page.slug
+        self.blog_submission_url = self.blog_index_page.url + self.blog_index_page.reverse_subpage('submit')
 
         self.blog_form_data = { 
             'date': '2015-01-01',
@@ -78,7 +78,7 @@ class MarketplaceEntryPageAddTests(TestCase):
 
     def test_marketplace_entry_form_valid(self):
         # This should successfully add a single new marketplace entry
-        resp = self.client.post(self.marketplace_submission_slug, self.marketplace_entry_form_data)
+        resp = self.client.post(self.marketplace_submission_url, self.marketplace_entry_form_data)
         self.assertEqual(resp.status_code, 302)
         self.assertEqual(resp['Location'], 'http://testserver/%s/submit-thank-you/' % self.marketplace_index_page.slug)
         self.assertEqual(MarketplaceEntryPage.objects.all().count(), 1)
@@ -86,14 +86,14 @@ class MarketplaceEntryPageAddTests(TestCase):
     def test_marketplace_entry_form_invalid_honeypot(self):
         # This should throw an ValidationError and not add the entry
         self.marketplace_entry_form_data['company_company_email'] = 'Iamabot@bots.com'
-        resp = self.client.post(self.marketplace_submission_slug, self.marketplace_entry_form_data)
+        resp = self.client.post(self.marketplace_submission_url, self.marketplace_entry_form_data)
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.context[0]['form'].errors['__all__'], ['Invalid submission'])
         self.assertEqual(MarketplaceEntryPage.objects.all().count(), 0)
 
     def test_case_study_form_valid(self):
         # This should successfully add a single new case study
-        resp = self.client.post(self.case_study_submission_slug, self.case_study_form_data)
+        resp = self.client.post(self.case_study_submission_url, self.case_study_form_data)
         self.assertEqual(resp.status_code, 302)
         self.assertEqual(resp['Location'], 'http://testserver/%s/submit-thank-you/'% self.case_study_index_page.slug)
         self.assertEqual(CaseStudyPage.objects.all().count(), 1)
@@ -103,35 +103,35 @@ class MarketplaceEntryPageAddTests(TestCase):
 
     def test_case_study_form_invalid_honeypot(self):        
         self.case_study_form_data['email_field_email'] = 'notnine'
-        resp = self.client.post(self.case_study_submission_slug, self.case_study_form_data)
+        resp = self.client.post(self.case_study_submission_url, self.case_study_form_data)
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.context[0]['form'].errors['__all__'], ['Invalid submission'])
         self.assertEqual(CaseStudyPage.objects.all().count(), 0)
 
     def test_case_study_invalid_bad_email(self):
         self.case_study_form_data['submitter_email'] = 'invalid@nope'
-        resp = self.client.post(self.case_study_submission_slug, self.case_study_form_data)
+        resp = self.client.post(self.case_study_submission_url, self.case_study_form_data)
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.context[0]['form'].errors['submitter_email'], ['Enter a valid email address.'])
         self.assertEqual(CaseStudyPage.objects.all().count(), 0)
 
     def test_blog_form_valid(self):
         # This should successfully add a single new blog post
-        resp = self.client.post(self.blog_submission_slug, self.blog_form_data)
+        resp = self.client.post(self.blog_submission_url, self.blog_form_data)
         self.assertEqual(resp.status_code, 302)
         self.assertEqual(resp['Location'], 'http://testserver/%s/submit-thank-you/'% self.blog_index_page.slug)
         self.assertEqual(BlogPage.objects.all().count(), 1)
 
     def test_blog_form_invalid_honeypot(self):        
         self.blog_form_data['email_field_email'] = 'notnine'
-        resp = self.client.post(self.blog_submission_slug, self.blog_form_data)
+        resp = self.client.post(self.blog_submission_url, self.blog_form_data)
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.context[0]['form'].errors['__all__'], ['Invalid submission'])
         self.assertEqual(BlogPage.objects.all().count(), 0)
 
     def test_blog_form_invalid_bad_email(self):
         self.blog_form_data['submitter_email'] = 'invalid@nope'
-        resp = self.client.post(self.blog_submission_slug, self.blog_form_data)
+        resp = self.client.post(self.blog_submission_url, self.blog_form_data)
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.context[0]['form'].errors['submitter_email'], ['Enter a valid email address.'])
         self.assertEqual(BlogPage.objects.all().count(), 0)
