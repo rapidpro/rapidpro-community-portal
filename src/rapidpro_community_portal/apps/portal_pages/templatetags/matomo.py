0aa735223e07ffab1ca0483a6d576281412ceb01
Domenico DiNicola <dom.dinicola@gmail.com>
2020-04-07 13:46:55 -0400

    remove breakpoint

--- src/rapidpro_community_portal/apps/portal_pages/templatetags/matomo.py
+++ src/rapidpro_community_portal/apps/portal_pages/templatetags/matomo.py
@@ -7,7 +7,6 @@ register = template.Library()
 
 @register.inclusion_tag('matomo.html')
 def matomo():
-    breakpoint()
     return {
         'site_tracker': settings.MATOMO_SITE_TRACKER,
         'site_id': str(settings.MATOMO_SITE_ID),
